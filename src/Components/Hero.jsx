74edbaf68c3f12895ced813334b0b66b8fd867c7
aroM_okoR <rocco.moramora@gmail.com>
2023-10-16 15:30:27 -0500

    Modified Hero for practice

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -7,7 +7,17 @@ import heroImg from "../media/hero_illustration.png";
 import CustomButton from "./CustomButton";
 
 const Hero = () => {
-  const CustomBox = styled(Box)(({ theme }) => ({}));
+  const CustomBox = styled(Box)(({ theme }) => ({
+    displey: "flex",
+    justifyContent: "center",
+    gap: theme.spacing(5),
+    MarginTop:theme.spacing(3),
+    [theme.breakpoints.down("sm")]:{
+      flexDirection: "column",
+      alignItems: "center",
+      textAlign: "center"
+    }
+  }));
 
   const Title = styled(Typography)(({ theme }) => ({}));
 
