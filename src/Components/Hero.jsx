547c05478d820168f3d69dd38ecf8bea3ce584e4
aroM_okoR <rocco.moramora@gmail.com>
2023-10-16 20:16:30 -0500

    Modify process

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -19,9 +19,49 @@ const Hero = () => {
     }
   }));
 
-  const Title = styled(Typography)(({ theme }) => ({}));
+  const Title = styled(Typography)(({ theme }) => ({
+    fontSize:"64px",
+    color:"#000336",
+    fontWehigt:"bold",
+    margin: theme.spacing(4,0,4,0),
+    [theme.breakpoints.down("sm")]:{
+      fontSize:"40px",
+    },
+  }));
 
-  return <></>;
+  return <Box sx={{backgroundColor: "#E6F0FF",minHeight:"80vh"}}>
+    <Container>
+      <Navbar/>
+      <CustomBox>
+        <Box sx={{flex:"1"}}>
+          <Typography variant="body2" 
+          sx={{
+            fontSize:"18px",
+            color:"#687690",
+            fontWehigt:"500",
+            mt:10,
+            mb:4,
+          }}
+          >
+            Welcome to Besnik Agencty
+          </Typography>
+          <Title variant="h1">
+            Discover a place where you'll love to live.
+          </Title>
+          <Typography variant="body2"
+          sx={{
+            fontSize:"18px",
+            color: "#5A6473",
+            my:4,
+          }}
+          >
+            Be the first to get the best real estate deals before they hit themass market! Hot foreclosure deals with one simple search!
+          </Typography>
+          <CustomButton backgroundColor="#0F1B4C" color="#fff" buttonText="More About Us" heroBtn={true}/>
+        </Box>
+      </CustomBox>
+    </Container>
+  </Box>;
 };
 
 export default Hero;
